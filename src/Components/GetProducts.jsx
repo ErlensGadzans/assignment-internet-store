d1887f8c69a0279720e28f665b454d0935f73a1f
Erlens <erlens.gadzans@gmail.com>
2021-07-01 16:55:49 +0300

    Fixed problem with data display
    Data was not displaying after page is refreshed.

--- src/Components/GetProducts.jsx
+++ src/Components/GetProducts.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
 import { LOAD_PRODUCTS } from "../GraphQL/Queries";
 
 export default function GetProducts() {
+  const [products, setProducts] = useState([]);
   const { error, loading, data } = useQuery(LOAD_PRODUCTS);
 
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      setProducts(data.category.products);
+    }
   }, [data]);
 
-  return <div></div>;
+  return (
+    <div>
+      {products.map((value, id) => (
+        <h1 key={id}>{value.name}</h1>
+      ))}
+    </div>
+  );
 }
